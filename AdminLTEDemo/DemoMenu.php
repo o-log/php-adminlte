6caa87c1b7d9a17bd49442c55d40699068cc752b
Антон Пинегин <pinegin.anton@gmail.com>
2016-11-01 16:43:13 +0300

    fix

--- AdminLTEDemo/DemoMenu.php
+++ AdminLTEDemo/DemoMenu.php
@@ -10,10 +10,10 @@ class DemoMenu implements InterfaceMenu
 {
     static public function menuArr(){
         return [
-            new MenuItem('123', '/', NULL, 'glyphicon glyphicon-th-list'),
+            new MenuItem('123', '/', [], 'glyphicon glyphicon-th-list'),
             new MenuItem('234', '', [
-                new MenuItem('345', '/2', NULL, 'glyphicon glyphicon-record'),
-                new MenuItem('456', '/admin', NULL, 'glyphicon glyphicon-record')
+                new MenuItem('345', '/2', [], 'glyphicon glyphicon-record'),
+                new MenuItem('456', '/admin', [], 'glyphicon glyphicon-record')
             ], 'glyphicon glyphicon-home'),
             new MenuItem('567', '', [
                 new MenuItem('678', '/4'),
